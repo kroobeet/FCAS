daa86ba0b5bd4a5f9654707515b63c5887878f73
kroobeet <kroobeet@gmail.com>
2025-05-17 18:50:09 +0600

    imports sys, psycopg2, PyQt6

---
+++ app.py
@@ -0,0 +1,9 @@
+import sys
+import psycopg2
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
+    QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem,
+    QTabWidget, QMessageBox, QComboBox, QCheckBox
+)
+from PyQt6.QtCore import Qt
+
